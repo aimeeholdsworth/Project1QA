5e0e52afea45677f53c96d84ffd1983a326e65d4
aimeeholdsworth <aimeeholdsworth@hotmail.co.uk>
2021-03-09 16:02:57 +0000

    Made Controller and service

--- src/main/java/repo/GigRepo.java
+++ src/main/java/repo/GigRepo.java
@@ -1,5 +1,14 @@
 package repo;
 
-public class GigRepo {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import domain.Gig;
+
+@Repository
+public interface GigRepo extends JpaRepository<Gig, Long>{
+	Gig findByArtist(String artist);
+	//Gig findById(Long id);
 }
+
+
---
+++ src/main/java/services/GigService.java
@@ -0,0 +1,83 @@
+package services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import domain.Gig;
+import repo.GigRepo;
+
+
+@Service
+public class GigService {
+	
+	//Link to the gig repo
+	private GigRepo repo;
+	
+	public GigService(GigRepo repo) {
+		super();
+		this.setRepo(repo);
+	}
+	
+	//set up the crud functions
+	
+	//create 
+	public Gig addGig(Gig gig) {
+		return this.repo.save(gig);
+	}
+	
+	//return
+	//return all
+	public List<Gig> getAllGigs(){
+		return this.repo.findAll();
+	}
+	
+	//return by artist
+	public Gig getGigByArtist(String artist) {
+		return this.repo.findByArtist(artist);
+	}
+	
+//	public Gig getGigById(Long id) {
+//		return this.repo.findById(id);
+//	}
+	
+	
+	//update
+	public Gig updateGig(Long id, Gig newGig) {
+	Optional <Gig> existingOptional = this.repo.findById(id);
+	Gig existing = existingOptional.get();
+	
+	//update the existing gig with new data
+	existing.setArtist(newGig.getArtist());
+	existing.setCity(newGig.getCity());
+	existing.setVenue(newGig.getVenue());
+	existing.setGigDate(newGig.getGigDate());
+	existing.setGigTime(newGig.getGigTime());
+	
+	//return the new data
+	return this.repo.save(existing);
+	}
+	
+	
+	//delete
+	public boolean removeGig(Long id) {
+		this.repo.deleteById(id);
+		boolean exists = this.repo.existsById(id);
+		return !exists; //returns true if successfully deleted
+	}
+	
+	//repo getters and setters
+	
+	
+	
+
+	public GigRepo getRepo() {
+		return repo;
+	}
+
+	public void setRepo(GigRepo repo) {
+		this.repo = repo;
+	}
+
+}
