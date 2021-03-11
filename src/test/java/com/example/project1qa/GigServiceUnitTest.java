96df4b81f599c949f3e25a553cf9d6ffc57f4198
aimeeholdsworth <aimeeholdsworth@hotmail.co.uk>
2021-03-11 13:25:11 +0000

    integration testing

---
+++ src/test/java/com/example/project1qa/GigServiceUnitTest.java
@@ -0,0 +1,5 @@
+package com.example.project1qa;
+
+public class GigServiceUnitTest {
+
+}
---
+++ src/test/java/com/example/project1qa/ProjectQaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project1qa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectQaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
